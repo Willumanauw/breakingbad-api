c636a1629f5686544ccb070a141c7cd2ccc601c3
Willumanauw <willumanauw@gmail.com>
2021-01-07 17:10:17 +0800

    create API

---
+++ src/components/ui/Search.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('')
+
+  const onChange = (q) => {
+    setText(q)
+    getQuery(q)
+  }
+  return (
+    <section className='search'>
+      <from>
+        <input
+          className='form-control'
+          type='text'
+          placeholder='Search characters'
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        />
+      </from>
+    </section>
+  )
+}
+
+export default Search
